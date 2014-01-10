60fa82663d71de0b9bb0cbeecef7045010019a29
hoppfrosch <hoppfrosch@gmx.de>
2014-01-10 10:29:30 +0100

    [*] Minor changes to make pylint happy

--- weaving/tablet/card.py
+++ weaving/tablet/card.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
+"""Implementation of Class <Card>"""
 
+# pylint: disable=W0611
 import __future__
 
 
-class Card:
+class Card(object):
     """
-    Card for tablet weaving
+    Definition of a card for tablet weaving
     """
     _max_holes = 6
 
-    """Definition of a card for tablet weaving"""
     def __init__(self, holes=4):
         """
         Constructor - generate new card for tablet weaving
@@ -19,6 +20,6 @@ class Card:
         # Validate number of holes
         if not isinstance(holes, int):
             raise TypeError("Number of holes can only be an integer")
-        if not(2 <= holes <= self._max_holes):
+        if not 2 <= holes <= self._max_holes:
             raise ValueError("Given Number of " + str(holes) + " holes must be in range [2.." + str(self._max_holes) + "]")
         self.holes = holes
