d7041f73f0367a7f205ff6dfca230257bd197796
hoppfrosch <hoppfrosch@gmx.de>
2014-01-10 11:47:53 +0100

    [+] Property <sz> - Storing SZ-Threading of a tablet

--- tests/TestWeavingTabletCard.py
+++ tests/TestWeavingTabletCard.py
@@ -23,6 +23,7 @@ class TestWeavingTabletCardHoles(unittest.TestCase):
 
         mycard = weaving.tablet.card.Card()
         self.assertEqual(mycard.holes, 4)
+        return
 
     def test_holes_2(self):
         """Successful test on 2 hole card"""
@@ -40,6 +41,38 @@ class TestWeavingTabletCardHoles(unittest.TestCase):
     def test_holes_string(self):
         """Failure test on unknown hole card (number of holes is not an integer)"""
         self.assertRaises(TypeError, weaving.tablet.card.Card, ("STRING"))
+
+
+class TestWeavingTabletCardSZ(unittest.TestCase):
+    """Unittest concerning joles of a card"""
+
+    def setUp(self):
+        self.test = 0
+
+    def test_s(self):
+        """Successful test on S-threaded card"""
+        mycard = weaving.tablet.card.Card()
+        self.assertEqual(mycard.sz, "S")
+
+    def test_z(self):
+        """Successful test on Z-threaded card"""
+        mycard = weaving.tablet.card.Card(SZ="Z")
+        self.assertEqual(mycard.sz, "Z")
+
+    def test_z_set(self):
+        """Successful test on Z-threaded card"""
+        mycard = weaving.tablet.card.Card()
+        self.assertEqual(mycard.sz, "S")
+        mycard.sz = "Z"
+        self.assertEqual(mycard.sz, "Z")
+
+    def test_wrongdatatype(self):
+        """Failure test on wrong SZ-Type (SZ is not a string)"""
+        self.assertRaises(TypeError, lambda: weaving.tablet.card.Card(SZ=1))
+
+    def test_wrongdatavalue(self):
+        """Failure test on wrong SZ-Type (SZ is not a string)"""
+        self.assertRaises(ValueError, lambda: weaving.tablet.card.Card(SZ="A"))
 
 
 if __name__ == '__main__':
--- weaving/tablet/card.py
+++ weaving/tablet/card.py
@@ -3,6 +3,10 @@
 
 # pylint: disable=W0611
 import __future__
+import re
+from version import Version
+
+__version__ = str(Version('0.1.0-alpha.1'))
 
 
 class Card(object):
@@ -11,11 +15,11 @@ class Card(object):
     """
     _max_holes = 6
 
-    def __init__(self, holes=4):
+    def __init__(self, holes=4, SZ="S"):
         """
         Constructor - generate new card for tablet weaving
 
-        @param    holes   Number of holes within the tabletin the sequence to return
+        @param    holes   Number of holes within the tablet
         """
         # Validate number of holes
         if not isinstance(holes, int):
@@ -23,3 +27,19 @@ class Card(object):
         if not 2 <= holes <= self._max_holes:
             raise ValueError("Given Number of " + str(holes) + " holes must be in range [2.." + str(self._max_holes) + "]")
         self.holes = holes
+        self.sz = SZ  # pylint: disable=C0103
+        return
+
+    @property
+    def sz(self):  # pylint: disable=C0103
+        """Getter for property SZ"""
+        return self._sz
+
+    @sz.setter
+    def sz(self, value):  # pylint: disable=C0103
+        """Setter for property SZ"""
+        if not isinstance(value, basestring):
+            raise TypeError("SZ can only be an string")
+        if not value is "S" and not value is "Z":
+            raise ValueError("SZ must either be S or Z")
+        self._sz = value  # pylint: disable=C0103
